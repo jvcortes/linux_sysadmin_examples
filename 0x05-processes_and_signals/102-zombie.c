4865f6272915b7d6a4759525fc037b03b7e46581
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-04 17:39:22 -0500

    (0x05: fix) task 11
    Fixed program making nested forks.

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 int infinite_while(void);
@@ -11,21 +12,17 @@ int infinite_while(void);
 int main(void)
 {
 	int i;
-	pid_t pid;
 
 	for (i = 0; i < 5; i++)
 	{
-		pid = fork();
-
-		if (pid > 0)
+		if (fork() == 0)
 		{
-			printf("Zombie process created, PID: %d\n", pid);
-			infinite_while();
+			printf("Zombie process created, PID: %d\n", getpid());
+			exit(0);
 		}
-		else
-			continue;
 	}
 
+	infinite_while();
 	return (0);
 }
 
