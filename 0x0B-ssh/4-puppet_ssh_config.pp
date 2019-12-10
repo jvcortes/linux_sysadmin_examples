0c5f3654a232dbe60e35501dd392248dc430bfb5
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-12-10 01:13:55 -0500

    (0x04: fix) task 4
    Fixed Puppet manifest by implementing the use of the file_line resource.

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # Edits ~/.ssh/config to refuse to authenticate to a server by its password
+include stdlib
 
-$file = file('/etc/ssh/ssh_config')
-$str = "Host 35.237.10.249\nIdentityFile ~/.ssh/holberton\n"
-$contents = $file
-
-unless $str in $file {
-  $_contents = "${contents}${str}"
+file_line { 'Turn off passwd auth':
+  path =>    '/etc/ssh/ssh_config',
+  line =>    'PasswordAuthentication no'
 }
 
-file { '/etc/ssh/ssh_config':
-  path    =>    '/etc/ssh/ssh_config',
-  content =>    $_contents
+file_line { 'Declare identity file':
+  path =>    '/etc/ssh/ssh_config',
+  line =>    'IdentityFile ~/.ssh/holberton'
 }
