d9267af5e3678c6179ad229851da243bbf2ff553
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-12-09 19:14:53 -0500

    (0x0A) task 2
    Created Puppet manifest for task 2.

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Kills the process with the name 'killmenow'
+
+exec { 'killall' :
+  path     =>   ['/usr/bin', '/sbin/', '/bin/', '/usr/sbin'],
+  provider =>   'shell',
+  command  =>   'killall killmenow'
+}
+
