3f1c598070cb8055f0cec001251b4229f219dc14
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-12-14 00:50:00 -0500

    (0x0C: fix) 7-puppet_install_nginx_web_server.pp
    changed use of file_line to exec with sed.

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,16 +11,16 @@ package { 'nginx' :
   name   =>  'nginx',
 }
 
-file { 'index.html' :
-  path    =>  '/var/www/html/index.html',
-  content =>  'Holberton School',
+exec { 'create index file' :
+  command  => 'echo "Holberton School" | sudo tee /var/www/html/index.html',
+  provider => 'shell'
 }
 
-file_line { 'set-redirect' :
-  ensure => present,
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => "\tserver_name _;",
-  line   => "  rewrite ^/redirect_me/?(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;"
+exec { 'set redirect' :
+  command  => "sudo sed -i '46 i \trewrite ^/redirect_me/?(.*)$  \
+  https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' \
+  /etc/nginx/sites-enabled/default",
+  provider => 'shell'
 }
 
 service { 'nginx' :
