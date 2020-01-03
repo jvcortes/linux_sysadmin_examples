5f08274e2c53d3f37b89b48805fd63ad51688fe0
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-02 22:51:29 -0500

    (0x0F) fix on task 2 idk

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,20 +1,25 @@
 # Configures a nginx custom header
 
+exec { 'update repository':
+  command  => 'sudo apt-get -y update',
+  provider => 'shell'
+} ->
+
 exec { 'install nginx' :
   command  => 'sudo apt-get -y install nginx',
   provider => 'shell'
-}
+} ->
 
 package { 'nginx' :
   ensure =>  'present',
   name   =>  'nginx'
-}
+} ->
 
 exec { 'set custom header' :
   command  => "sudo sed -i \"47 i \tadd_header X-Served-By $(hostname);\"\
   /etc/nginx/sites-enabled/default",
   provider => 'shell'
-}
+} ->
 
 service { 'nginx' :
   ensure => 'running',
