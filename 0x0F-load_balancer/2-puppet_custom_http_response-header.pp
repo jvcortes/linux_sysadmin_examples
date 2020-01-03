a3a7dbc74c30180a2c22d03ca93020879fc159ed
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-02 22:11:11 -0500

    (0x0F) fix on task 2
    Added missing semicolon.

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,8 +1,7 @@
 # Configures a nginx custom header
 
-
 exec { 'set custom header' :
-  command  => "sudo sed -i \"47 i \tadd_rule X-Served-By $(hostname)\"\
+  command  => "sudo sed -i \"47 i \tadd_rule X-Served-By $(hostname);\"\
   /etc/nginx/sites-enabled/default",
   provider => 'shell'
 }
