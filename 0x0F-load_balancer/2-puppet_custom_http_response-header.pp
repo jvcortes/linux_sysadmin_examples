fc817ef7b163635f858893002a08944adaecd7cb
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-02 22:13:07 -0500

    (0x0F) fix on task 2
    Added tasks for nginx installation and service management.

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,22 @@
 # Configures a nginx custom header
 
+exec { 'install nginx' :
+  command  => 'sudo apt-get -y install nginx',
+  provider => 'shell'
+}
+
+package { 'nginx' :
+  ensure =>  'present',
+  name   =>  'nginx',
+}
+
 exec { 'set custom header' :
   command  => "sudo sed -i \"47 i \tadd_rule X-Served-By $(hostname);\"\
   /etc/nginx/sites-enabled/default",
   provider => 'shell'
+}
+
+service { 'nginx' :
+  ensure => 'running',
+  name   => 'nginx'
 }
