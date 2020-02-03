ebe270bbae2470dd42fe09a4dc7baf866837f22d
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-03 18:58:16 -0500

    (0x15) moving 0x15 from higher-level-programming

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Gathers and prints some data from https://jsonplaceholder.typicode.com/
+import sys
+import requests
+
+todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                     params={'userId': sys.argv[1]}).json()
+
+completed = list(filter(lambda todo: todo["completed"], todos))
+
+user = requests.get("https://jsonplaceholder.typicode.com/users",
+                    params={'id': sys.argv[1]}).json()[0]
+
+
+print("Employee {} is done with tasks({}/{}):".format(
+    user["name"],
+    len(completed),
+    len(todos)))
+
+for todo in completed:
+    print('\t', end='')
+    print(todo["title"])
