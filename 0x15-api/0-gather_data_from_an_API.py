d85c7ea26b4b4555cd3c8684e2418f21bd98c370
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-03 19:03:29 -0500

    (0x15) fix on task 0
    Added import protection.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 # Gathers and prints some data from https://jsonplaceholder.typicode.com/
-import sys
-import requests
+if __name__ == '__main__':
+    import sys
+    import requests
 
-todos = requests.get("https://jsonplaceholder.typicode.com/todos",
-                     params={'userId': sys.argv[1]}).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                         params={'userId': sys.argv[1]}).json()
 
-completed = list(filter(lambda todo: todo["completed"], todos))
+    completed = list(filter(lambda todo: todo["completed"], todos))
 
-user = requests.get("https://jsonplaceholder.typicode.com/users",
-                    params={'id': sys.argv[1]}).json()[0]
+    user = requests.get("https://jsonplaceholder.typicode.com/users",
+                        params={'id': sys.argv[1]}).json()[0]
 
+    print("Employee {} is done with tasks({}/{}):".format(
+        user["name"],
+        len(completed),
+        len(todos)))
 
-print("Employee {} is done with tasks({}/{}):".format(
-    user["name"],
-    len(completed),
-    len(todos)))
-
-for todo in completed:
-    print('\t', end='')
-    print(todo["title"])
+    for todo in completed:
+        print('\t ', end='')
+        print(todo["title"])
