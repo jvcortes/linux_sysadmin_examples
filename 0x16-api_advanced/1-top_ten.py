d63d2c3f500d5e8c1e2dd1450bfd1a96b3b2789a
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-06 19:29:22 -0500

    (0x16) task 1
    Created function definition for task 1.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+# Module for task 1
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 'Hot' posts from a subreddit.
+
+    If the given subreddit is not found, the function will print "None".
+
+    Arguments:
+        subreddit (str): subreddit to search.
+    """
+
+    request = requests.get("https://www.reddit.com/r/{}/hot.json"
+                           "?limit=10".format(subreddit),
+                           allow_redirects=False,
+                           headers={'User-Agent': '941'})
+
+    if request.status_code == 301:
+        print("None")
+
+    posts = request.json()["data"]["children"]
+    for post in posts:
+        print(post["data"]["title"])
